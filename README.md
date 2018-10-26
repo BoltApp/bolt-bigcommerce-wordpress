b900aa8e4847e9a8936f85aebf253aa019622ec1
daisy1754 <d-part@hotmail.co.jp>
2018-10-25 20:50:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Bolt plugin for BigCommerce for Wordpress.
---
+++ bolt-bigcommerce-wordpress.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Bolt Checkout for BigCommerce for WooCommerce.
+ *
+ * @link              http://bolt.com
+ * @since             1.0.0
+ *
+ * Plugin Name:       Bolt for BigCommerce Wordpress
+ * Plugin URI:        http://bolt.com
+ * Description:       It adds Bolt checkout to the BigCommerce Wordpress.
+ * Version:           0.0.1
+ * Author:            Bolt
+ * Author URI:        https://bolt.com
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
