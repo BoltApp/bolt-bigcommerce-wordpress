156eaadd4b116074a321aacad63590321fb4a939
Vitaliy Reznikov <vitaliy@reznikov.ru>
2018-11-28 14:14:55 +0000

    Add travis CI support (like in Bolt-Woocommerce)

---
+++ tests/bin/check_reports.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# check if report folder exist.
+if [ -d /home/travis/build/BoltApp/bolt-woocommerce/var/report/ ]
+then
+    echo "${red} ### Reports folder present ### ${reset}"
+    cd /home/travis/build/BoltApp/bolt-woocommerce/var/report/
+    for file in ./*; do
+        cat "$(basename "$file")"
+        echo "\n"
+    done
+else
+    echo "${green} ### Reports folder not present ### ${reset}"
+fi
