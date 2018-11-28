cb955c6ea0a2518a00826374cc426a965e265b40
Vitaliy Reznikov <vitaliy@reznikov.ru>
2018-11-28 16:30:44 +0000

    Add fake root file for bigcommerce

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,7 @@ if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
 
+
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
 	exit( 1 );
@@ -41,6 +42,13 @@ function _manually_load_plugin() {
 
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+echo "*".$_tests_dir,"*";
+if (!file_exists($_tests_dir.'/plugins')) mkdir($_tests_dir.'/plugins');
+if (!file_exists($_tests_dir.'/plugins/bigcommerce-for-wordpress'))mkdir($_tests_dir.'/plugins/bigcommerce-for-wordpress' );
+$file = fopen($_tests_dir.'/plugins/bigcommerce-for-wordpress/bigcommerce.php','w');
+fwrite ($file,"<?php>");
+fclose ($file);
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
